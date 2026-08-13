889abd7c9fc5ec3e8be78c6873880d1c3615c7fc
plutoyxuanan0112-a <plutoyxuanan0112@gmail.com>
2026-08-13 16:47:10 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'yijian-pwa-disabled-v35';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
